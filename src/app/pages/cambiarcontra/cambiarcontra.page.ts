6bbe67dcd677e683e682bddb4f9b844b61f64876
NaTheFunny <matiasbetorr@gmail.com>
2024-08-27 11:32:32 -0400

    Marketplace v1.0.4

--- src/app/pages/cambiarcontra/cambiarcontra.page.ts
+++ src/app/pages/cambiarcontra/cambiarcontra.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cambiarcontra',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CambiarcontraPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  iniciarses(){
+    this.router.navigate(['/login']);
   }
 
 }
